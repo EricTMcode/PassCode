c50c909f385871020e69bca41836ce30aa284f45
Eric <ebagnaro@gmail.com>
2024-02-05 06:05:09 +0100

    Initial Commit

---
+++ PassCode/PassCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassCodeApp.swift
+//  PassCode
+//
+//  Created by Eric on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PassCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
